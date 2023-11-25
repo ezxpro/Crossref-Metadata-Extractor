f8083351b56d78dd5dad3c2c7e614011272c0216
ezxpro <ezequiel.ezb@gmail.com>
2023-11-24 22:28:39 -0300

    add README

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# crossref-metadata-fetcher
-This script gets the ISSN of a journal and fetches the DOI, title or any other metadata set in it and stores it in a JSON file.
+# CROSSREF Journal DOI Retriever
+
+-------------------------
+
+This is supposed to receive the ISSN of a journal as an input and return a JSON file with metadata from it, including the name, the issue number and publication date.
+
+I'm using the Crossref Unified API for this purpose, it's a public, free API, and as long as you don't thrash their servers with too many requests AND provide your email in your queries (so they can contact you if you mess up), you can use it freely.
+
+To modify the queries and to get the fields you want you'll need to take a look into [the Crossref API documentation]\(https://api.crossref.org/swagger-ui/index.html#/Journals/get_journals__issn__works\) and change the requests in the code as you like, it should be pretty straightforward to use.
+
+This is very rough around the edges, I wrote this because I intend to create a list of DOI number and title of every article in every issue of a given journal.
+
+I'm just publishing this because one day it might be useful to someone, so there's no interactivity here, the ISSN and the JSON file name are hardcoded, but it should be easy to modify this script to make it interactive and maybe pass the output file path and the ISSN as parameters. Otherwise, just open the python script, specify the ISSN (either as a single number or separated by a dash) and the JSON output file and run the script, it should be fine.
---
+++ crossref_to_json.py
@@ -0,0 +1,59 @@
+import requests
+import json
+import time
+
+json_name = "your_json_name.json"
+# Define the base URL
+issn="your_ISSN"
+url = f"https://api.crossref.org/journals/{issn}/works"
+
+# Define the headers
+headers = {
+    # they want you to identify yourself when you use the API. Don't be an asshole and follow the guidelines
+    # they can be found here: https://github.com/CrossRef/rest-api-doc#good-manners--more-reliable-service
+    "User-Agent": "YOUR_USER_AGENT (YOUR_WEBSITE_ADDRESS; YOUR_EMAIL_HERE)"
+}
+
+# Define the parameters
+params = {
+    "rows": 1000,  # Change this to the number of records you want per page
+    "mailto": "YOUR_EMAIL_HERE,
+    "select": "DOI,author,title,issue,issued",
+    "sort": "published",
+    "order": "asc",
+    "cursor": "*"  # Start with cursor set to *
+}
+
+# Initialize a list to store all items
+all_items = []
+
+while True:
+    # Send the GET request with the parameters
+    response = requests.get(url, headers=headers, params=params)
+    data = response.json()
+
+    # Get the items from the response
+    items = data['message']['items']
+
+    # Check if any items were returned
+    if not items:
+        # No items, break the loop
+        break
+
+    # Add the items to the list and print them
+    for item in items:
+        print(f"DOI: {item['DOI']}, Title: {item['title'][0]}, Issue: {item.get('issue', 'N/A')}, Issued: {item['issued']['date-parts'][0]}")
+        all_items.append(item)
+
+    # Write the items to a JSON file
+    with open(json_name, 'w') as f:
+        json.dump(all_items, f, indent=4)
+
+    # Update the cursor parameter for the next page
+    params['cursor'] = data['message']['next-cursor']
+
+    # Pause for 10 seconds to be polite to the server. 
+    print("Waiting 10 seconds...")
+    time.sleep(10)
+
+print(f"Total items retrieved: {len(all_items)}")
